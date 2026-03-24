95b9c1d926d57364579379ed43052fd56f4663bf
김규린 <koong5111@naver.com>
2026-03-25 01:32:44 +0900

    [0325] 김규린

---
+++ wordcount/wordcount_prj/wordcount/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def word_count(request):
+    return render(request, 'word_count.html')
+
+def result(request):
+    #요청이 들어오면 fulltext 가져오기
+    entered_text = request.GET['fulltext']
+    #entered_text 공백 기준 문자열로 나누기
+    word_list = entered_text.split()
+    
+    #글자수
+    text_len = len(entered_text)
+    text_len_n = len(entered_text.replace(" ", ""))
+    
+    word_dictionary = {}
+    
+    #{word : 출현횟수}
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+    
+    max_count = max(word_dictionary.values())
+
+    max_word = [word for word, count in word_dictionary.items() if count == max_count]
+    
+    word_count = len(word_dictionary)
+    
+    #result.html에 변수 전달
+    return render(request, 'result.html', {'alltext': entered_text, 'dictionary': word_dictionary.items(), 'text_len':text_len,
+                                        'text_len_n':text_len_n,'max_word':max_word,'max_count':max_count,'word_count':word_count })
+
+def hello(request):
+    entered_text = request.GET['name']
+    return render(request, 'hello.html', {'name': entered_text})
---
+++ wordcount/wordcount_prj/wordcount_prj/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from wordcount import views
+
+urlpatterns = [
+    #, 꼭 작성하기, url 오타 XX
+    path("admin/", admin.site.urls),
+    path('', include('wordcount.urls')),
+]
