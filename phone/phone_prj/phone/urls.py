a68c163a495a7b78d87a8f591dc934c4b6bff1e6
김규린 <koong5111@naver.com>
2026-04-01 14:14:07 +0900

    [0401] 주소록 만들기 김규린

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views 
+from .views import list, create, result
+#from .views import * :path가 덮어쓰여질 수 있음 -> 명시적으로 import!
+#from . import views : views.list, views.create, views.result로 작성
 
 app_name = "phone"
 
 urlpatterns = [
-    path("", views.list, name="list"),
-    path("create/", views.create, name="create"),
-    path("result/", views.result, name="result"),
+    path("", list, name="list"),
+    path("create/", create, name="create"),
+    path("result/", result, name="result"),
 ]
