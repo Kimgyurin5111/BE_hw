35119c575c14a7717815605c0490bf4a41ec55c9
김규린 <koong5111@naver.com>
2026-04-02 19:14:59 +0900

    [0402] 스터디 김규린

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list, create, result
+from .views import list, create, result, detail, delete, update
 #from .views import * :path가 덮어쓰여질 수 있음 -> 명시적으로 import!
 #from . import views : views.list, views.create, views.result로 작성
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path("", list, name="list"),
     path("create/", create, name="create"),
     path("result/", result, name="result"),
+    path("detail/<int:id>/",detail, name="detail"),
+    path("delete/<int:id>/", delete, name="delete"),
+    path("update/<int:id>/", update, name="update"),
+    
 ]
--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -14,13 +14,34 @@ def create(request):
         phone = Phone.objects.create(
             name = name,
             phone_num = phone_num,
-            email = email,
+            email = email
         )
         return redirect('phone:list')
     return render(request, "phone/create.html")
 
 def result(request):
-    keyword = request.GET.get("keyword","")
+    keyword = request.GET.get("keyword")
     phones = Phone.objects.filter(name__contains=keyword).order_by('name')
     
     return render(request, "phone/result.html", {"phones": phones, "keyword": keyword})
+
+def detail(request,id):
+    phone = Phone.objects.get(id=id)
+    return render(request, "phone/detail.html", {"phone": phone})
+
+def delete(request,id):
+    phone = Phone.objects.get(id=id)
+    if request.method == "POST":
+        phone.delete()
+        return redirect('phone:list')
+    return render(request, "phone/delete.html", {"phone": phone})
+
+def update(request,id):
+    phone = Phone.objects.get(id=id)
+    if request.method == "POST":
+        phone.name = request.POST.get("name")
+        phone.phone_num = request.POST.get("phone_num")
+        phone.email = request.POST.get("email")
+        phone.save()
+        return redirect('phone:detail', id=id)
+    return render(request, "phone/update.html", {"phone": phone})
