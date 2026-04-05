aa1e1a5bf7cbc91cbed67d83887e3c16c4f56791
김규린 <koong5111@naver.com>
2026-04-05 22:32:47 +0900

    [0405] 스터디 과제 김규린

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import list, create, result, detail, delete, update
+from .views import IndexView, create, result, detail, delete, update
 #from .views import * :path가 덮어쓰여질 수 있음 -> 명시적으로 import!
 #from . import views : views.list, views.create, views.result로 작성
 
 app_name = "phone"
 
 urlpatterns = [
-    path("", list, name="list"),
+    path("", IndexView.as_view(), name="list"),
     path("create/", create, name="create"),
     path("result/", result, name="result"),
     path("detail/<int:id>/",detail, name="detail"),
