5d8db4c453c3f0d92a739affd188570f3ba21ec0
kimgyurin <koong5111@naver.com>
2026-04-28 17:36:36 +0900

    auto-commit

--- blog/blog_prj/blog/views.py
+++ blog/blog_prj/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.decorators import login_required
 
 def list(request):
@@ -40,4 +40,18 @@ def update(request, id):
 def delete(request, id):
     post = get_object_or_404(Post, id=id)
     post.delete()
+    return redirect('blog:list')
+
+@login_required
+def create_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        
+        Comment.objects.create(
+            post=post,
+            content=content,
+            author=request.user
+        )
+        return redirect('blog:detail', post_id)
     return redirect('blog:list')
